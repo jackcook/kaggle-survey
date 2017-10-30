5416f72d74b9744cf6eed9123697464ecc668a40
Jack Cook <hello@jackcook.nyc>
2017-10-30 19:19:54 -0400

    Added a theme to the graph

--- main.r
+++ main.r
@@ -19,11 +19,25 @@ for (platform in platforms) {
 }
 
 scores_df <- data.frame(
-    popularity = popularities,
-    usefulness = scores,
-    name = names
+    Popularity = popularities,
+    Usefulness = scores,
+    Name = names
 )
 
-ggplot(scores_df, aes(x=usefulness, y=popularity)) +
+ggplot(scores_df, aes(x = Usefulness, y = Popularity)) +
+    ggtitle("Effectiveness of Learning Methods") +
     geom_point() +
-    geom_text(aes(label=name), vjust = 0)
+    geom_smooth(method = lm, se = FALSE, color = "#333333", size = 0.5) +
+    geom_text(aes(label = Name), nudge_y = 225) +
+    theme(
+        plot.background = element_rect(fill = "#eeeeee"),
+        panel.background = element_rect(fill = "#eeeeee"),
+        panel.grid.major = element_line(size = 0.4, linetype = "solid", color = "#cccccc"),
+        panel.grid.minor = element_line(size = 0),
+        plot.title = element_text(size = 20, family = "San Francisco Display", face = "bold", hjust = 0.5, margin = margin(b = 20)),
+        axis.title = element_text(size = 14, family = "San Francisco Display", face = "bold"),
+        axis.title.x = element_text(margin = margin(t = 20)),
+        axis.title.y = element_text(margin = margin(r = 20)),
+        axis.ticks = element_blank(),
+        plot.margin = unit(c(1, 1.25, 1, 1), "cm")
+    )
